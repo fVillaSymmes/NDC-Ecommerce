1a405a43a278b44e93947adb1deacc5a7d6ef500
Felipe Villarrubia <felipevillarrubiasymmes@gmail.com>
2024-06-13 23:38:47 -0400

    Se elimina menuHamburguesa y se crea LinkActivo. Se logra dar con el funcionamiento del menu hamburguesa, pero requiere correcciones especificadas en comentarios en navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,13 @@
+'use client'
+//Idealmente el navbar completo no debería ser use client. Solo los trocitos que así lo requieran
+import { useRef } from 'react'
+import { LinkActivo } from '../LinkActivo/LinkActivo'
+
+const navItems = [{ text: 'Tienda' }, { text: 'Contacto' }]
+
 export const Navbar = () => {
+    // Hay que determinar un tipo adecuado para el useRef aquí
+    const menuHamburguesa = useRef<any>(null)
     return (
         <nav className='bg-white border border-gray-200 dark:border-gray-700 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 shadow'>
             <div className='container flex flex-wrap justify-between items-center mx-auto'>
@@ -13,6 +22,15 @@ export const Navbar = () => {
                         id='menu-toggle'
                         type='button'
                         className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden'
+                        onClick={() =>
+                            // ¿Qué es este onClick funcional pero ordinarísimo?
+                            menuHamburguesa.current.className ==
+                            'w-full md:block md:w-auto hidden'
+                                ? (menuHamburguesa.current.className =
+                                      'w-full md:block md:w-auto')
+                                : (menuHamburguesa.current.className =
+                                      'w-full md:block md:w-auto hidden')
+                        }
                     >
                         <span className='sr-only'>Open main menu</span>
                         <svg
@@ -22,9 +40,9 @@ export const Navbar = () => {
                             stroke='currentColor'
                         >
                             <path
-                                stroke-linecap='round'
-                                stroke-linejoin='round'
-                                stroke-width='2'
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                strokeWidth='2'
                                 d='M4 6h16M4 12h16m-7 6h7'
                             />
                         </svg>
@@ -34,49 +52,13 @@ export const Navbar = () => {
                 <div
                     className='w-full md:block md:w-auto hidden'
                     id='mobile-menu'
+                    ref={menuHamburguesa}
+                    //Este ref es por ahora el trozo del código que me evita hacer el navbar 'use Server'
                 >
                     <ul className='flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium'>
-                        <li>
-                            <a
-                                href='#'
-                                className='block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
-                                aria-current='page'
-                            >
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href='#'
-                                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                            >
-                                About
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href='#'
-                                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                            >
-                                Services
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href='#'
-                                className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                            >
-                                Pricing
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href='#'
-                                className='block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                            >
-                                Contact
-                            </a>
-                        </li>
+                        {navItems.map((navItem) => (
+                            <LinkActivo key={navItem.text} {...navItem} />
+                        ))}
                     </ul>
                 </div>
             </div>
