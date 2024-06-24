56f694f58ab65ec07f2b0b9aede342693b94b1ca
Felipe Villarrubia <felipevillarrubiasymmes@gmail.com>
2024-06-23 22:30:10 -0400

    se corrige grosor de los bordes de visualización de la página fechas para que no entorpezcan dimensiones.

--- src/app/(general)/fechas/page.tsx
+++ src/app/(general)/fechas/page.tsx
@@ -1,26 +1,26 @@
 export default function Fechas() {
   return (
-    <main className='container mx-auto border-4 border-red-500'>
+    <main className='container mx-auto border border-red-500'>
       <h1 className='text-center text-5xl'>Página de fechas</h1>
-      <ul className='border-4 border-green-500 py-3'>
-        <li className='flex flex-col items-center gap-y-3 border-2 border-yellow-300'>
+      <ul className='border border-green-500 py-3'>
+        <li className='flex flex-col items-center gap-y-3 border border-yellow-300'>
           <h2>01 de Agosto</h2>
           <p>Antofagasta</p>
-          <button className='rounded-none border-4 border-blue-900'>
+          <button className='rounded-none border border-blue-900'>
             Entradas
           </button>
         </li>
-        <li className='flex flex-col items-center gap-y-3 border-2 border-yellow-300'>
+        <li className='flex flex-col items-center gap-y-3 border border-yellow-300'>
           <h2>02 de Agosto</h2>
           <p>Iquique</p>
-          <button className='rounded-none border-4 border-blue-900'>
+          <button className='rounded-none border border-blue-900'>
             Entradas
           </button>
         </li>
-        <li className='flex flex-col items-center gap-y-3 border-2 border-yellow-300'>
+        <li className='flex flex-col items-center gap-y-3 border border-yellow-300'>
           <h2>03 de Agosto</h2>
           <p>Arica</p>
-          <button className='rounded-none border-4 border-blue-900'>
+          <button className='rounded-none border border-blue-900'>
             Entradas
           </button>
         </li>
